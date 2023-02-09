35b876a577c5ff4d430713eef4475aab80148989
AlphaQuinn <snowboarderquinn@gmail.com>
2023-02-09 16:26:35 -0500

    added classes and tests

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,8 @@
+const Employee = require("./lib/Employee");
+describe("testing employee class construction", ()=>{
+    it ("should add a name to the constructor",()=>{
+        var name = "Sam";
+        const obj = new Employee (name);
+        expect (obj.name).toEqual(name);
+    });
+});
